341313d39dee7f1739c029a5a3ad812ec8f24b25
Yauhen Sampir <yauhen_sampir@epam.com>
2018-05-18 17:27:31 +0300

    initial implementation

---
+++ ShoppingCart/ShoppingCart/ShoppingCartModule.cs
@@ -0,0 +1,34 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+
+namespace ShoppingCart.ShoppingCart
+{
+    public class ShoppingCartModule : NancyModule
+    {
+        public ShoppingCartModule(
+            IShoppingCartStore shoppingCartStore,
+            IProductcatalogClient productcatalog,
+            IEventStore eventStore) : base("/shoppingcart")
+        {
+            Get(
+                "/{userid:int}",
+                parameters =>
+                {
+                    var userId = (int)parameters.userid;
+                    return shoppingCartStore.Get(userId);
+                });
+            Post("/{userid:int}/items",
+                async (parameters, _) =>
+                {
+                    var productcatalogIds = this.Bind<int[]>();
+                    var userId = (int)parameters.userid;
+
+                    var shoppingCart = shoppingCartStore.Get(userId);
+                    var shoppingCartItems = await productcatalog.GetShoppingCartItems(productcatalogIds)
+                        .ConfigureAwait(false);
+                    shoppingCart.AddItems(shoppingCartItems, eventStore);
+                    shoppingCartStore.Save(shoppingCart);
+                });
+        }
+    }
+}
